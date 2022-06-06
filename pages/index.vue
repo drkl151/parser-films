4662b58a887e133dcef878a31dd0b63b34c5d550
ol_kuznetsova <booking@tickets.ua>
2022-06-07 01:36:55 +0300

    added new design and api

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,84 @@
-/* eslint-disable vue/order-in-components */
 <template>
   <main id="main">
-    <Table :films="films" />
+    <Loader v-if="isRequest" />
+
+    <template v-else>
+      <div v-if="!isRequest && !films.length" class="getFilms">
+        <p>Выберите категорию :</p>
+
+        <select v-model="currentPage" class="select-film">
+          <option
+            v-for="page of pages"
+            :key="page.title"
+            class="select-film__option"
+            :value="page"
+          >
+            {{ page.title }}
+          </option>
+        </select>
+
+        <button class="getFilms__btn" @click="parseHdrezka(currentPage.url)">
+          Получить фильмы
+        </button>
+      </div>
+
+      <Table v-if="films.length" :films="films" />
+    </template>
   </main>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 import Table from '@/components/Table/Table'
+import Loader from '@/components/Loader/Loader.vue'
 
 export default {
   name: 'IndexPage',
 
   components: {
     Table,
+    Loader,
   },
 
-  mounted() {
-    this.parse()
+  data() {
+    return {
+      pages: [
+        {
+          title: 'Последние поступления',
+          url: 'https://hdrezka.ag/?filter=last&genre=1',
+        },
+        {
+          title: 'Популярные',
+          url: 'https://hdrezka.ag/?filter=popular&genre=1',
+        },
+        {
+          title: 'Сейчас смотрят',
+          url: 'https://hdrezka.ag/?filter=watching&genre=1',
+        },
+      ],
+      currentPage: {
+        title: 'Сейчас смотрят',
+        url: 'https://hdrezka.ag/?filter=watching&genre=1',
+      },
+    }
   },
 
   // eslint-disable-next-line vue/order-in-components
   computed: {
     ...mapGetters({
-      films: 'parse/films',
+      isRequest: 'hdrezka/isRequest',
+      films: 'hdrezka/films',
     }),
   },
 
   methods: {
     ...mapActions({
-      parse: 'parse/parse',
+      parseHdrezka: 'hdrezka/parse',
     }),
   },
 }
 </script>
 
-<style lang="css">
-body {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-#main {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  padding: 40px 20px;
-  width: 100vw;
-  height: 100vh;
-}
+<style lang="scss">
+@import '../style/index.scss';
 </style>
--- store/parse.js
+++ store/parse.js
@@ -1,80 +1,80 @@
-import axios from 'axios'
-import cheerio from 'cheerio'
+// import axios from 'axios'
+// import cheerio from 'cheerio'
 
-// import getTorrents from '../utils/getTorrents.js'
+// // import getTorrents from '../utils/getTorrents.js'
 
 export const state = () => ({
   films: [],
 })
 
-export const actions = {
-  parse({ commit, dispatch }) {
-    const urlSite = 'https://kinobase.org/films?sort=viewed_week'
+// export const actions = {
+//   parse({ commit, dispatch }) {
+//     const urlSite = 'https://kinobase.org/films?sort=viewed_week'
 
-    axios
-      .get(urlSite)
-      .then(({ data }) => {
-        const $ = cheerio.load(data)
-        const allFilms = []
+//     axios
+//       .get(urlSite)
+//       .then(({ data }) => {
+//         const $ = cheerio.load(data)
+//         const allFilms = []
 
-        // получение массива популярных фильмов
-        $('.col-xs-2').each((i, el) => {
-          const $element = $(el)
+//         // получение массива популярных фильмов
+//         $('.col-xs-2').each((i, el) => {
+//           const $element = $(el)
 
-          allFilms.push(
-            // eslint-disable-next-line no-undef
-            `https://kinobase.org/${$element.find('a').attr('href')}`
-          )
-        })
+//           allFilms.push(
+//             // eslint-disable-next-line no-undef
+//             `https://kinobase.org/${$element.find('a').attr('href')}`
+//           )
+//         })
 
-        // парсинг каждого фильма отдельно
-        return allFilms
-      })
-      .then((films) => {
-        const allParseFilms = []
+//         // парсинг каждого фильма отдельно
+//         return allFilms
+//       })
+//       .then((films) => {
+//         const allParseFilms = []
 
-        films.forEach((film) => {
-          axios.get(film).then(({ data }) => {
-            const $ = cheerio.load(data)
-            const filmPoster = $('.data.clearfix .poster img').attr('src')
+//         films.forEach((film) => {
+//           axios.get(film).then(({ data }) => {
+//             const $ = cheerio.load(data)
+//             const filmPoster = $('.data.clearfix .poster img').attr('src')
 
-            const infoBlock = $('.list-unstyled.details li')
-            const name = $('h1')
-            const alternativeName = $('h4.alternative_title')
-            const description = $('div.body[itemprop="description"]')
-            const fullInfo = {
-              Название: name.text(),
-              АльтернативноеНазвание: alternativeName.text(),
-              Постер: filmPoster,
-              Описание: description.text(),
-            }
+//             const infoBlock = $('.list-unstyled.details li')
+//             const name = $('h1')
+//             const alternativeName = $('h4.alternative_title')
+//             const description = $('div.body[itemprop="description"]')
+//             const fullInfo = {
+//               Название: name.text(),
+//               АльтернативноеНазвание: alternativeName.text(),
+//               Постер: filmPoster,
+//               Описание: description.text(),
+//             }
 
-            infoBlock.each((i, el) => {
-              const $element = $(el)
-              const splitElement = $element.text().split(': ')
+//             infoBlock.each((i, el) => {
+//               const $element = $(el)
+//               const splitElement = $element.text().split(': ')
 
-              fullInfo[splitElement[0]] = splitElement[1].trim()
-            })
+//               fullInfo[splitElement[0]] = splitElement[1].trim()
+//             })
 
-            allParseFilms.push(fullInfo)
-          })
-        })
+//             allParseFilms.push(fullInfo)
+//           })
+//         })
 
-        return allParseFilms
-      })
-      .then((films) => {
-        setTimeout(() => {
-          commit('setFilms', films)
-        }, 1300)
-      })
-  },
-}
+//         return allParseFilms
+//       })
+//       .then((films) => {
+//         setTimeout(() => {
+//           commit('setFilms', films)
+//         }, 1300)
+//       })
+//   },
+// }
 
-export const mutations = {
-  setFilms(state, films) {
-    state.films = films
-  },
-}
+// export const mutations = {
+//   setFilms(state, films) {
+//     state.films = films
+//   },
+// }
 
 export const getters = {
   films: ({ films }) => films,
